b8eea75884d24ff5eb0eef929608986db6ab710a
Arvin Alvea <arvinalvea@gmail.com>
2021-05-12 11:31:29 -0400

    Update script.js
    Updated with personal comments

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -1,16 +1,5 @@
 /*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-/*
-Create the `showPage` function
+`showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
@@ -41,20 +30,18 @@ function showPage(list, page) {
 }
 
 /*
-Create the `addPagination` function
+`addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
 function addPagination(list) {
-    // create a variable to calculate the number of pages needed
     let numOfpages = Math.ceil(list.length / 9);
-    // select the element with a class of `link-list` and assign it to a variable
+
     let linkList = document.querySelector(".link-list");
-    // set the innerHTML property of the variable you just created to an empty string
+
+    // Removes any existing pagination buttons that might have been displayed previously
     linkList.innerHTML = "";
-    // loop over the number of pages needed
-    // create the elements needed to display the pagination button
-    // insert the above elements
+
     for (let i = 1; i <= numOfpages; i++) {
         let button = `
       <li>
@@ -63,9 +50,12 @@ function addPagination(list) {
       `;
         linkList.insertAdjacentHTML("beforeend", button);
     }
+
+    // Adds "active" class to first button
     let firstButton = document.querySelector("button:first-of-type");
     firstButton.className = "active";
 
+    // Adds "active" class to button that is click and removes "active" class from previously active button
     linkList.addEventListener("click", function (e) {
         if ((e.target.tagName = "button")) {
             let activeButton = document.querySelector(".active");
@@ -77,16 +67,7 @@ function addPagination(list) {
             return;
         }
     });
-
-    // give the first pagination button a class of "active"
-
-    // create an event listener on the `link-list` element
-    // if the click target is a button:
-    // remove the "active" class from the previous button
-    // add the active class to the clicked button
-    // call the showPage function passing the `list` parameter and page to display as arguments
 }
 
-// Call functions
 showPage(data, 1);
 addPagination(data);
